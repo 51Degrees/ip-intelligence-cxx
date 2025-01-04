4544195fdeb5b04ac12fa86f0e8a38bce8eb8094
Automation51D <51DCI@51Degrees.com>
2025-01-04 00:54:46 +0000

    Update documentation

---
+++ 4.4/navtreedata.js
@@ -0,0 +1,47 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+    [ "General Docs", "../../documentation/4.4/index.html", null ],
+    [ "51Degrees Ip Intelligence API", "md__home_runner_work_ip-intelligence-cxx_ip-intelligence-cxx_common_ip-intelligence-cxx__r_e_a_d_m_e.html", null ],
+    [ "51Degrees Common Code Library", "md__home_runner_work_ip-intelligence-cxx_ip-intelligence-cxx_common_ip-intelligence-cxx_src_common-cxx__r_e_a_d_m_e.html", null ],
+    [ "C Modules", "modules.html", "modules" ],
+    [ "C++ Namespaces", "namespaces.html", "namespaces" ]
+];
+
+var NAVTREEHIDEROOT = true;
+
+var NAVTREEHIDESYNC = true;
+
+var NAVTREEINDEX =
+[
+"../../documentation/4.4/index.html",
+"group___fifty_one_degrees_properties.html#ga382d04480940ed108153d1090f08acb1",
+"group___fifty_one_degrees_synonyms.html#ga82823fbf1c466f7d9f8233d309bd7c99",
+"md__home_runner_work_ip-intelligence-cxx_ip-intelligence-cxx_common_ip-intelligence-cxx__r_e_a_d_m_e.html",
+"structfiftyone_degrees_override_value_array.html#a5aa7de9d612be54cf6ac69b6157eb513"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
