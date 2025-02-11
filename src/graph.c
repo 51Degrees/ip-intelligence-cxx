9d1f6d6371e3a0176e09b900f6ca2d0f6061730f
James Rosewell <james@51degrees.com>
2025-02-11 16:45:27 +0000

    DOC: Added TODO for patent notices.

--- src/graph.c
+++ src/graph.c
@@ -18,6 +18,9 @@
  * including the attribution notice(s) required under Article 5 of the EUPL
  * in the end user terms of the application under an appropriate heading, 
  * such notice(s) shall fulfill the requirements of that article.
+ *
+ * [TODO INSERT PATENT NOTICE]
+ * 
  * ********************************************************************* */
 
 #include "graph.h"
