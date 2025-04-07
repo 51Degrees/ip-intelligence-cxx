4bd80538b8fdafe77c512106011928b847f48e9b
Maksym Kucherov <maximus.the0@gmail.com>
2025-04-07 18:47:53 +0300

    CI: Use `fetch-assets.ps1` from ipi-dotnet-examples.

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,14 +1,29 @@
-
+[CmdletBinding()]
 param (
     [string]$RepoName,
     [Parameter(Mandatory=$true)]
     [string]$DeviceDetection,
     [string]$DeviceDetectionUrl
 )
+$ErrorActionPreference = "Stop"
+$PSNativeCommandUseErrorActionPreference = $true
 
 # Fetch the TAC data file for testing with
 $DataFileName = "TAC-IpIntelligenceV41.ipi"
-./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl -DataType "IpIntelligenceV41" -ArchiveName "$DataFileName.gz"
+
+# TODO: Use `fetch-hash-assets.ps1`
+# ./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $DeviceDetection -Url $DeviceDetectionUrl -DataType "IpIntelligenceV41" -ArchiveName $DataFileName
+$ArchivedName = "51Degrees-EnterpriseIpiV41.ipi 1"
+$ArchiveName = "$ArchivedName.gz"
+Invoke-WebRequest -Uri $DeviceDetectionUrl -OutFile $RepoName/$ArchiveName
+$ArchiveHash = (Get-FileHash -Algorithm MD5 -Path $RepoName/$ArchiveName).Hash
+Write-Output "MD5 (fetched $ArchiveName) = $ArchiveHash"
+Write-Output "Extracting $ArchiveName"
+./steps/gunzip-file.ps1 $RepoName/$ArchiveName
+Move-Item -Path $RepoName/$ArchivedName -Destination $RepoName/$DataFileName
+
+$DataFileHash = (Get-FileHash -Algorithm MD5 -Path $RepoName/$DataFileName).Hash
+Write-Output "MD5 (fetched $DataFileName) = $DataFileHash"
 
 # Move the data file to the correct location
 $DataFileSource = [IO.Path]::Combine($pwd, $RepoName, $DataFileName)
@@ -20,14 +35,19 @@ Move-Item $DataFileSource $DataFileDestination
 # But are not pulled by default.
 Push-Location $DataFileDir
 try {
-    Write-Output "Pulling evidence files"
-    git lfs pull -I "*.csv" 
-    git lfs pull -I "*.yml"
+    # Write-Output "Pulling evidence files"
+    # git lfs pull
+
+    # Use Enterprise as Lite
+    Copy-Item $DataFileName 51Degrees-LiteV41.ipi
+
+    foreach ($NextIpiFile in (Get-ChildItem "*.ipi" | ForEach-Object { $_.Name })) {
+        $IpiFileHash = (Get-FileHash -Algorithm MD5 -Path $NextIpiFile).Hash
+        Write-Output "MD5 ($NextIpiFile) = $IpiFileHash"
+    }
+
+    ./evidence-gen.ps1 -v4 10000 -v6 10000
 }
 finally {
     Pop-Location
-}
-
-if ($IsLinux) {
-    ls -l $DataFileDir
 }
