b1fd14b7955c2c9298bbee8abe8e1b62303d497c
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-02 15:40:26 +0300

    FIX: Property name for performance tests.

--- examples/C/IpIntelligence/Performance.c
+++ examples/C/IpIntelligence/Performance.c
@@ -528,7 +528,7 @@ void executeBenchmark(
 
 	PropertiesRequired properties = PropertiesDefault;
 	if (config.allProperties == false) {
-		properties.string = "name,areas";
+		properties.string = "RegisteredName,Areas";
 	}
 
 	// // Multi graph operation is being deprecated. There is only one graph.
