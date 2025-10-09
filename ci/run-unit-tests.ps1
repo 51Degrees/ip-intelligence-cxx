5ddfa62c5692f330dcabac91257e2c3b05a81b0d
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-10-09 21:18:08 +0200

    run ctest in parallel

--- ci/run-unit-tests.ps1
+++ ci/run-unit-tests.ps1
@@ -7,6 +7,10 @@ param(
     [string]$BuildMethod = "cmake"
 )
 
+# Enable parallel test execution to speed up CI (836 tests)
+# Use -j 2 to balance speed vs memory usage (InMemory tests load 2.3GB data files)
+$env:CTEST_PARALLEL_LEVEL = 2
+
 ./cxx/run-unit-tests.ps1 `
     -RepoName $RepoName `
     -ProjectDir $ProjectDir `
