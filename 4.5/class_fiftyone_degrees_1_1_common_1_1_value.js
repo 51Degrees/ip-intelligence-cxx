8808a0b2b1abe9a319a3daea6a778db42513f73f
Automation51D <51DCI@51Degrees.com>
2026-06-23 04:20:44 +0000

    Update documentation from main

--- 4.5/class_fiftyone_degrees_1_1_common_1_1_value.js
+++ 4.5/class_fiftyone_degrees_1_1_common_1_1_value.js
@@ -1,11 +1,11 @@
 var class_fiftyone_degrees_1_1_common_1_1_value =
 [
     [ "Value", "class_fiftyone_degrees_1_1_common_1_1_value.html#a11980ff143c95bc2a542d1fa3c6ce0f8", null ],
-    [ "getNoValueMessage", "class_fiftyone_degrees_1_1_common_1_1_value.html#a155fdf390691e62d924ff9e9c07d225f", null ],
-    [ "getNoValueReason", "class_fiftyone_degrees_1_1_common_1_1_value.html#a93325ac4a38c32d2094a202a7d9d613c", null ],
-    [ "getValue", "class_fiftyone_degrees_1_1_common_1_1_value.html#ab796b2fc2a451c78e39d7029354dae84", null ],
-    [ "hasValue", "class_fiftyone_degrees_1_1_common_1_1_value.html#ac7831003d50002c31e78f5573cfc6e6c", null ],
-    [ "operator*", "class_fiftyone_degrees_1_1_common_1_1_value.html#ae3c2b359c22103a65120fb1bfa5453e2", null ],
+    [ "getNoValueMessage", "class_fiftyone_degrees_1_1_common_1_1_value.html#a903c9dca8ceb92dbc18b2e548d422dc9", null ],
+    [ "getNoValueReason", "class_fiftyone_degrees_1_1_common_1_1_value.html#ad0972ed9b19e483772a352d4f0f906c8", null ],
+    [ "getValue", "class_fiftyone_degrees_1_1_common_1_1_value.html#a6a68e3a555ecb61563f345f8ea8cbf2f", null ],
+    [ "hasValue", "class_fiftyone_degrees_1_1_common_1_1_value.html#adfbfda8989b3f28a1a779b45a32b6cf2", null ],
+    [ "operator*", "class_fiftyone_degrees_1_1_common_1_1_value.html#ace46633dcd72ada94dd0a7c87a89b958", null ],
     [ "setNoValueReason", "class_fiftyone_degrees_1_1_common_1_1_value.html#afa25e192ba35c6baefc3ae990a3896de", null ],
     [ "setValue", "class_fiftyone_degrees_1_1_common_1_1_value.html#a1ffa6541c889a835e81454a68b620b53", null ]
 ];
